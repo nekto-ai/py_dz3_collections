0514ca1723259b077889b4d24a13f42a8b36c1e9
Boris T <stud.neural@gmail.com>
2024-07-23 18:25:36 -0700

    mudule 2

---
+++ 2seq.py
@@ -0,0 +1,25 @@
+# MODULE 2
+
+def get_sequence():
+    sequence = input("Enter a sequence of whole numbers separated by comma: ")
+    return [int(num) for num in sequence.split(',')]
+
+our_list = get_sequence()
+print(f"Entered list: {our_list}")
+
+our_list.sort()
+print(f"Sorted list: {our_list}")
+
+duplicate_indexes = []
+for i in range(len(our_list)):
+    if our_list[i] == our_list[i-1] and i != 0:
+        duplicate_indexes.append(i-1)
+        duplicate_indexes.append(i)
+
+result_list = []
+for j in range(len(our_list)):
+    if j not in duplicate_indexes:
+        result_list.append(our_list[j])
+
+
+print(f"Resulted unique elements: {result_list}")
